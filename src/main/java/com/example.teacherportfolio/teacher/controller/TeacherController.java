5ad5b7ad55b92f75aca6fa294922e9ae433354f7
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-06-09 15:59:02 +0300

    Добавила аннотацию для связи с фронтендом, удалила лишние эндпоинты update

--- src/main/java/com/example.teacherportfolio/teacher/controller/TeacherController.java
+++ src/main/java/com/example.teacherportfolio/teacher/controller/TeacherController.java
@@ -6,17 +6,16 @@ import com.example.teacherportfolio.teacher.service.TeacherService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("teachers")
+@CrossOrigin(origins = "http://localhost:63342")
 @Slf4j
 public class TeacherController {
     private final TeacherService teacherService;
@@ -24,7 +23,7 @@ public class TeacherController {
     @GetMapping
     @Transactional
     public List<TeacherShortResponseDto> getAllTeachers() {
-        log.info("Получение списка препподавателей");
+        log.info("Получение списка преподавателей");
         return teacherService.getAllTeachers();
     }
 
@@ -55,52 +54,8 @@ public class TeacherController {
     public TeacherShortResponseDto update(
             @PathVariable Long id,
             @Valid @RequestBody TeacherRequestDto teacherDtoFull) {
-        log.info("Обновление преподавателя {}{}{}",teacherDtoFull.getLastName(), teacherDtoFull.getFirstName(), teacherDtoFull.getSurName());
+        log.info("Обновление преподавателя {} {} {}",teacherDtoFull.getLastName(), teacherDtoFull.getFirstName(), teacherDtoFull.getSurName());
         return teacherService.update(id, teacherDtoFull);
-    }
-
-    @PatchMapping("/{id}/name")
-    @Transactional
-    public TeacherShortResponseDto updateTeacherName(@PathVariable Long id,
-                                               @RequestParam String name) {
-        log.info("Обновление имени у преподавателя {}", id);
-        return teacherService.updateTeacherName(id, name);
-    }
-
-    @PatchMapping("/{id}/firstname")
-    @Transactional
-    public TeacherShortResponseDto updateTeacherFirstName(@PathVariable Long id,
-                                                    @RequestParam String firstName) {
-        log.info("Обновление фамилии у преподавателя {}", id);
-        return teacherService.updateTeacherFirsName(id, firstName);
-    }
-
-
-    @PatchMapping("/{id}/surname")
-    @Transactional
-    public TeacherShortResponseDto updateTeacherSurName(@PathVariable Long id,
-                                                  @RequestParam String surName) {
-        log.info("Обновление отчества у преподавателя {}", id);
-        return teacherService.updateTeacherSurName(id, surName);
-    }
-
-
-    @PatchMapping("/{id}/birthday")
-    @Transactional
-    public TeacherShortResponseDto updateTeacherDateOfBirth(@PathVariable Long id,
-                                                      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate dateOfBirth) {
-        log.info("Обновление даты рождения у преподавателя {}", id);
-        return teacherService.updateTeacherBirthday(id, dateOfBirth);
-    }
-
-    @PatchMapping("/{id}/partTime")
-    @Transactional
-    public TeacherShortResponseDto updateTeacherPartTimeStatus(
-            @PathVariable Long id,
-            @RequestParam Boolean isPartTime
-    ) {
-        log.info("Обновление статуса совместителя для преподавателя {}: {}", id, isPartTime);
-        return teacherService.updateTeacherPartTimeStatus(id, isPartTime);
     }
 
     @DeleteMapping("/{id}")
