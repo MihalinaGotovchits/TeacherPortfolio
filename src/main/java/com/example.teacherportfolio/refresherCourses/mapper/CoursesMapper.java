e866f8c4df5b458b191a07252fe795c3b8409811
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-06-10 14:05:55 +0300

    Изменила названия методов и возвращаемые типы

--- src/main/java/com/example.teacherportfolio/refresherCourses/mapper/CoursesMapper.java
+++ src/main/java/com/example.teacherportfolio/refresherCourses/mapper/CoursesMapper.java
@@ -1,45 +1,37 @@
 package com.example.teacherportfolio.refresherCourses.mapper;
 
-import com.example.teacherportfolio.refresherCourses.dto.CoursesDto;
-import com.example.teacherportfolio.refresherCourses.dto.CoursesDtoShort;
+import com.example.teacherportfolio.refresherCourses.dto.CoursesRequestDto;
+import com.example.teacherportfolio.refresherCourses.dto.CoursesResponseDto;
 import com.example.teacherportfolio.refresherCourses.model.Course;
+import com.example.teacherportfolio.teacher.model.Teacher;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class CoursesMapper {
-    public static CoursesDto toRefreshCoursesDto(Course refresherCourses) {
-        return CoursesDto.builder()
-                .id(refresherCourses.getId())
-                .nameOfCourse(refresherCourses.getNameOfCourse())
-                .countOfHours(refresherCourses.getCountOfHours())
-                .startOfTraining(refresherCourses.getStartOfTraining())
-                .endOfTraining(refresherCourses.getEndOfTraining())
-                .nameOfEducationalInstitution(refresherCourses.getNameOfEducationalInstitution())
-                .numberOfCertificate(refresherCourses.getNumberOfCertificate())
-                .dateOfCertificate(refresherCourses.getDateOfCertificate())
-                .teachers(refresherCourses.getTeachers())
-                .build();
-    }
-
-    public static CoursesDtoShort toRefreshCoursesDtoShort(Course refresherCourses) {
-        return CoursesDtoShort.builder()
-                .id(refresherCourses.getId())
-                .nameOfCourse(refresherCourses.getNameOfCourse())
-                .countOfHours(refresherCourses.getCountOfHours())
-                .build();
-    }
-
-    public static Course toRefreshCourses(CoursesDto refreshCoursesDto) {
+    public static Course toEntity(CoursesRequestDto dto, Teacher teacher) {
         return Course.builder()
-                .id(refreshCoursesDto.getId())
-                .nameOfCourse(refreshCoursesDto.getNameOfCourse())
-                .countOfHours(refreshCoursesDto.getCountOfHours())
-                .startOfTraining(refreshCoursesDto.getStartOfTraining())
-                .endOfTraining(refreshCoursesDto.getEndOfTraining())
-                .nameOfEducationalInstitution(refreshCoursesDto.getNameOfEducationalInstitution())
-                .numberOfCertificate(refreshCoursesDto.getNumberOfCertificate())
-                .dateOfCertificate(refreshCoursesDto.getDateOfCertificate())
-                .teachers(refreshCoursesDto.getTeachers())
+                .courseName(dto.getCourseName())
+                .countOfHours(dto.getHours())
+                .startDate(dto.getStartDate())
+                .endDate(dto.getEndDate())
+                .organization(dto.getOrganization())
+                .certificateNumber(dto.getCertificateNumber())
+                .certificateDate(dto.getCertificateDate())
+                .teacher(teacher)
+                .build();
+    }
+
+    public static CoursesResponseDto toDto(Course course) {
+        return CoursesResponseDto.builder()
+                .id(course.getId())
+                .courseName(course.getCourseName())
+                .hours(course.getCountOfHours())
+                .startDate(course.getStartDate())
+                .endDate(course.getEndDate())
+                .organization(course.getOrganization())
+                .certificateNumber(course.getCertificateNumber())
+                .certificateDate(course.getCertificateDate())
+                .teacherId(course.getTeacher().getId())
                 .build();
     }
 }
