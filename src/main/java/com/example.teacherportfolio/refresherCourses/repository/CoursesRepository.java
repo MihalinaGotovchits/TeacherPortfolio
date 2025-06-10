aa9f7568016ba7ac9cd0177906c2a3ceb207d700
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-06-10 14:09:35 +0300

    Убрала неиспользуемые импорты

--- src/main/java/com/example.teacherportfolio/refresherCourses/repository/CoursesRepository.java
+++ src/main/java/com/example.teacherportfolio/refresherCourses/repository/CoursesRepository.java
@@ -1,15 +1,8 @@
 package com.example.teacherportfolio.refresherCourses.repository;
 
 import com.example.teacherportfolio.refresherCourses.model.Course;
-import com.example.teacherportfolio.teacher.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface CoursesRepository extends JpaRepository<Course, Long> {
