22c328bb4c0fa2584442997e3b24dc57263dcd9c
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-06-10 14:06:18 +0300

    Удалила ненужные методы

--- src/main/java/com/example.teacherportfolio/refresherCourses/repository/CoursesRepository.java
+++ src/main/java/com/example.teacherportfolio/refresherCourses/repository/CoursesRepository.java
@@ -12,13 +12,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface CoursesRepository extends JpaRepository<Course, UUID> {
-    // Найти всех преподавателей на курсе (через связь ManyToMany)
-    @Query("SELECT t FROM Teacher t JOIN t.courses c WHERE c.id = :courseId")
-    List<Teacher> findTeachersByCourseId(@Param("courseId") UUID courseId);
+public interface CoursesRepository extends JpaRepository<Course, Long> {
 
-    @Query("SELECT c FROM Course c JOIN c.teachers t " +
-            "WHERE t.id = :teacherId AND c.nameOfCourse = :nameOfCourse")
-    Optional<Course> findByTeacherIdAndCourseName(@Param("teacherId") UUID teacherId,
-                                                  @Param("nameOfCourse") String nameOfCourse);
 }
